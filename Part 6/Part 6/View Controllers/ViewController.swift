c0c00a48ffdddfd30a394ae83d5fe79f4f05061c
eilyin <evgen.ilyin@gmail.com>
2020-03-13 17:11:46 +0300

    Add Part 6

---
+++ Part 6/Part 6/View Controllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Part 6
+//
+//  Created by Eugene Ilyin on 13.03.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: - View life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ Part 6/Part 6/Views/MetalView.swift
@@ -0,0 +1,48 @@
+//
+//  MetalView.swift
+//  Part 6
+//
+//  Created by Eugene Ilyin on 13.03.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+import UIKit
+
+// IN ORDER TO TEST PROJECT IN SIMULATOR UNCOMMENT NEXT LINE
+// @available(iOS 13.0, *)
+class MetalView: UIView {
+    
+    // MARK: - Properties
+    
+    var commandQueue: MTLCommandQueue!
+    
+    // In order to
+    var metalLayer: CAMetalLayer!
+    
+    // MARK: - UIView methods
+    
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
+        
+        metalLayer = CAMetalLayer()
+        metalLayer.device = MTLCreateSystemDefaultDevice()
+        metalLayer.frame = layer.frame
+        layer.addSublayer(metalLayer)
+        commandQueue = metalLayer.device?.makeCommandQueue()
+        redraw()
+    }
+    
+    // MARK: - Methods
+    
+    private func redraw(){
+        guard let drawable = metalLayer.nextDrawable() else { return }
+        let descriptor = MTLRenderPassDescriptor()
+        descriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0, green: 0.5, blue: 0.5, alpha: 1)
+        descriptor.colorAttachments[0].texture = drawable.texture
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: descriptor)
+        commandEncoder?.endEncoding()
+        commandBuffer?.present(drawable)
+        commandBuffer?.commit()
+    }
+}
