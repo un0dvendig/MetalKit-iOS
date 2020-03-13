13edcf3bc3ca810002afb8f289748a0e94859c5c
eilyin <evgen.ilyin@gmail.com>
2020-03-13 23:16:17 +0300

    Add Part 08

---
+++ Part 08/Part 08/Models/Vertex.swift
@@ -0,0 +1,17 @@
+//
+//  Vertex.swift
+//  Part 08
+//
+//  Created by Eugene Ilyin on 13.03.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+import simd
+
+struct Vertex {
+    
+    // MARK: - Properties
+    
+    var position: simd_float4
+    var color: simd_float4
+}
