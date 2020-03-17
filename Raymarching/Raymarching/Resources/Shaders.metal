d73d862b1c8259fab41a66e19550c80cf43a989d
eilyin <evgen.ilyin@gmail.com>
2020-03-17 13:19:53 +0300

    Add Raymarching

---
+++ Raymarching/Raymarching/Resources/Shaders.metal
@@ -0,0 +1,90 @@
+//
+//  Shaders.metal
+//  Raymarching
+//
+//  Created by Eugene Ilyin on 14.03.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+//  MARK: - Objects
+
+struct Ray {
+    float3 origin;
+    float3 direction;
+    
+    Ray(float3 o, float3 d) {
+        origin = o;
+        direction = d;
+    }
+};
+
+struct Sphere {
+    float3 center;
+    float radius;
+    
+    Sphere(float3 c, float r) {
+        center = c;
+        radius = r;
+    }
+};
+
+// MARK: - Distance functions
+
+float dist(float2 point, float2 center, float radius) {
+    return length(point - center) - radius;
+}
+
+float distToSphere(Ray ray, Sphere sphere) {
+    return length(ray.origin - sphere.center) - sphere.radius;
+}
+
+float distToScene(Ray ray) {
+    Sphere sphere = Sphere(float3(1.), 0.5);
+    Ray repeatRay = ray;
+    repeatRay.origin = fmod(ray.origin, 2.);
+    return distToSphere(repeatRay, sphere);
+}
+
+// MARK: - Compute
+
+kernel void compute(texture2d<float, access::write> output [[ texture(0) ]],
+                    constant float &timer [[ buffer(1) ]],
+                    constant float2 &touch [[ buffer(2) ]],
+                    uint2 gid [[ thread_position_in_grid ]]) {
+    int width = output.get_width();
+    int height = output.get_height();
+    
+    float2 uv = float2(gid) / float2(width, height);
+    uv = uv * 2. - 1.;
+    
+    // iOS workaround
+    if (width > height) {
+        uv.x /= .5;
+    } else {
+        uv.y /= .5;
+    }
+    
+    
+    float3 camPos = float3(1000. + sin(timer) + 1.,
+                           1000. + cos(timer) + 1.,
+                           timer);
+    Ray ray = Ray(camPos,
+                  normalize(float3(uv, 1.0)));
+    float3 color = float3(0.);
+    // Increase max `i` to increase output image quality
+    for (int i = 0; i < 100.; i++) {
+        float dist = distToScene(ray);
+        if (dist < .001) {
+            color = float3(1.);
+            break;
+        }
+        ray.origin += ray.direction * dist;
+    }
+    
+    float3 posRelativeToCamera = ray.origin - camPos;
+    
+    output.write(float4(color * abs(posRelativeToCamera / 10. ), 1.), gid);
+}
---
+++ Raymarching/Raymarching/Views/MetalMTKView.swift
@@ -0,0 +1,108 @@
+//
+//  MetalMTKView.swift
+//  Raymarching
+//
+//  Created by Eugene Ilyin on 13.03.2020.
+//  Copyright © 2020 Eugene Ilyin. All rights reserved.
+//
+
+import MetalKit
+
+// FOR SOME REASON THIS PROJECT PRODUCES VISUAL BUG
+// (RED LINE) ON THE SIDE OF THE SIMULATOR / REAL DEVICE
+public class MetalMTKView: MTKView {
+    
+    // MARK: - Properties
+    
+    var computePipelineState: MTLComputePipelineState!
+    var touchBuffer: MTLBuffer!
+    var pos: CGPoint!
+    var timer: Float = 0
+    var timerBuffer: MTLBuffer!
+    var queue: MTLCommandQueue!
+    
+    // MARK: - Initialization
+    
+    required public init(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        ///  `!IMPORTANT!`
+        framebufferOnly = false
+        
+        guard let device = MTLCreateSystemDefaultDevice() else { return }
+        self.device = device
+        guard let queue = device.makeCommandQueue() else { return }
+        self.queue = queue
+        
+        registerShaders(for: device)
+    }
+
+    
+    // MARK: - MTKView methods
+    
+    override public func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        if let drawable = currentDrawable,
+            let commandBuffer = queue.makeCommandBuffer(),
+            let commandEncoder = commandBuffer.makeComputeCommandEncoder() {
+            
+            commandEncoder.setComputePipelineState(computePipelineState)
+            commandEncoder.setTexture(drawable.texture, index: 0)
+            commandEncoder.setBuffer(timerBuffer, offset: 0, index: 1)
+            commandEncoder.setBuffer(touchBuffer, offset: 0, index: 2)
+            update()
+            
+            let threadGroupCount = MTLSize(width: 8, height: 8, depth: 1)
+            let threadGroups = MTLSize(width: drawable.texture.width / threadGroupCount.width,
+                                       height: drawable.texture.height / threadGroupCount.height,
+                                       depth: 1)
+            
+            commandEncoder.dispatchThreadgroups(threadGroups,
+                                                threadsPerThreadgroup: threadGroupCount)
+            commandEncoder.endEncoding()
+            commandBuffer.present(drawable)
+            commandBuffer.commit()
+        }
+    }
+    
+    // MARK: - Touches
+    
+    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        pos = touch.location(in: self)
+        let scale = layer.contentsScale
+        pos.x *= scale
+        pos.y *= scale
+    }
+    
+    // MARK: - Private methods
+        
+    private func registerShaders(for device: MTLDevice) {
+        do {
+            guard let library = device.makeDefaultLibrary(),
+                let kernel = library.makeFunction(name: "compute"),
+                let timerBuffer = device.makeBuffer(length: MemoryLayout<Float>.size, options: []),
+                let touchBuffer = device.makeBuffer(length: MemoryLayout<CGPoint>.size, options: []) else {
+                    return
+                    
+            }
+            
+            self.timerBuffer = timerBuffer
+            self.touchBuffer = touchBuffer
+            computePipelineState = try device.makeComputePipelineState(function: kernel)
+            
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    private func update() {
+        timer += 0.01
+        var bufferPointer = timerBuffer.contents()
+        bufferPointer.copyMemory(from: &timer, byteCount: MemoryLayout<Float>.size)
+        
+        bufferPointer = touchBuffer.contents()
+        bufferPointer.copyMemory(from: &pos, byteCount: MemoryLayout<CGPoint>.size)
+    }
+}
